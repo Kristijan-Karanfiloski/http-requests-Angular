e53a3fa5b860c8749c630e6b470c519621d1fa68
Kristijan <kikaccc29@gmail.com>
2023-05-18 20:10:40 +0200

    "first-commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+
 import { Subscription } from 'rxjs';
 
 import { Post } from './post.model';
